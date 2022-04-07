ad1bdec9132ffb4ca97560c0948002451c6a4fea
kabajie <Kabaji.Egara@derivco.co.uk>
2022-04-07 14:24:50 +0100

    added navigation component using Outlet

--- src/components/category-menu/category-menu.component.jsx
+++ src/components/category-menu/category-menu.component.jsx
@@ -1,7 +1,7 @@
 import CategoryItem from "../category-item/category-item.component";
 import "./category-menu.styles.scss";
 
-const CategoryMenu = (categories) => {
+const CategoryMenu = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map((category) => {
