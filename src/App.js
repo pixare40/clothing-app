52897991d796f65829ec4f69cba5a76d1e1a9d7e
kabajie <Kabaji.Egara@derivco.co.uk>
2022-04-25 11:11:48 +0100

    made firebase util method a promis

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+  getCurrentUser,
+} from "./utils/firebase/firebase.utils";
 import Home from "./components/routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Authentication from "./components/routes/sign-in/authentication.component";
 import Shop from "./components/routes/shop/shop.component";
 import Checkout from "./components/routes/checkout/checkout.component";
-import { setCurrentUser } from "./store/user/user.action";
 
 function App() {
-
-  const dispatch = useDispatch();
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user));
   }, []);
 
   return (
