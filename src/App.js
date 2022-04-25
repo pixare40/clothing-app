6d4904fde7ca0f66a45df8f74d357eaf35b686c6
kabajie <Kabaji.Egara@derivco.co.uk>
2022-04-25 16:59:19 +0100

    added lazy and suspense, fixed tests

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-  getCurrentUser,
-} from "./utils/firebase/firebase.utils";
-import Home from "./components/routes/home/home.component";
+
+//import Home from "./components/routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Authentication from "./components/routes/sign-in/authentication.component";
 import Shop from "./components/routes/shop/shop.component";
 import Checkout from "./components/routes/checkout/checkout.component";
 import { checkUserSession } from "./store/user/user.action";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = lazy(() => import("./components/routes/home/home.component"));
 
 function App() {
   const dispatch = useDispatch();
@@ -21,14 +20,16 @@ function App() {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/store/user/tests/user.reducer.test.js
+++ src/store/user/tests/user.reducer.test.js
@@ -3,14 +3,18 @@ import { USER_ACTION_TYPES } from "../user.types";
 
 describe("userReducer", () => {
   it("should return the initial state", () => {
-    expect(userReducer(undefined, {})).toEqual({ currentUser: null });
+    expect(userReducer(undefined, {})).toEqual({
+      currentUser: null,
+      isLoading: false,
+      error: null,
+    });
   });
   it("should update state when a valid action is passed", () => {
     expect(
       userReducer(undefined, {
-        type: USER_ACTION_TYPES.SET_CURRENT_USER,
+        type: USER_ACTION_TYPES.SIGN_IN_SUCCESS,
         payload: "abc",
       })
-    ).toEqual({ currentUser: "abc" });
+    ).toEqual({ currentUser: "abc", isLoading: false, error: null });
   });
 });
