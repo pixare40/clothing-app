2307188badea5e418e63f7264c3c6c799cdb1e0e
kabajie <Kabaji.Egara@derivco.co.uk>
2022-04-25 15:15:47 +0100

    added memo on cart item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,19 @@
-import './cart-item.styles.scss'
+import { memo } from "react";
+import "./cart-item.styles.scss";
 
-const CartItem = ({ cartItem }) => {
-    const { name, quantity, imageUrl, price } = cartItem;
-    return (
-        <div className='cart-item-container'>
-            <img src={imageUrl} alt={`${name}`} />
-            <div className='item-details'>
-                <span className='name'>{name}</span>
-                <span className='price'>{quantity} x ${price}</span>
-
-            </div>
-        </div>
-    )
-
-}
+const CartItem = memo(({ cartItem }) => {
+  const { name, quantity, imageUrl, price } = cartItem;
+  return (
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+});
 
 export default CartItem;
